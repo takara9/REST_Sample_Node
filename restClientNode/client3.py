808ba773e64da6e2aba3afe31d5776fc1c743c30
Maho Takara <tkr@test57.takara.org>
2017-12-27 17:11:04 +0900

    update

--- restClientNode/client3.py
+++ restClientNode/client3.py
@@ -3,9 +3,13 @@
 #
 #  RESTクライアント
 #
+
+import threading
 import requests
+import time as tm
 from requests.auth import HTTPBasicAuth
-#url = 'https://nodehashxx.mybluemix.net'
+gcnt = 0
+url = 'http://10.132.253.44'
 
 def work(url):
     # GET
@@ -24,7 +28,7 @@ def work(url):
     resp = requests.post(uri,json,auth=HTTPBasicAuth(username, password))
 
     # 結果表示
-    rslt = resp.json()
+    ##rslt = resp.json()
     #print "POST"
     #print "status_code = ", resp.status_code
     #print "sha1   = ", rslt['sha1']
@@ -39,14 +43,47 @@ def work(url):
     #print "resp = ", resp.text
     #print
 
-
-if __name__ == '__main__':
-    
-    url = 'http://127.0.0.1'
+def para_main():
+    global gcnt
     n = 32
     while True:
         for p in range(0,n):
             portno = 3000 + p
-            #print "p=",portno
             uri = url + ":" + str(portno)
             work(uri)
+            gcnt += 1
+
+
+def show_rate():
+    global gcnt
+    t0 = tm.time()
+    while True:
+        t1 = tm.time()
+        tps = gcnt/(t1-t0)
+        print t0, gcnt, tps
+        t0 = t1
+        gcnt = 0
+        tm.sleep(5)
+        
+
+if __name__ == '__main__':
+    
+    #scale = 5
+    #scale = 10
+    #scale = 15
+    #scale = 20
+    scale = 25
+
+    thread = [0] * scale
+    show = threading.Thread(target=show_rate)
+
+    for i in range(scale):
+        thread[i] = threading.Thread(target=para_main)
+
+    show.start()
+
+    for i in range(scale):
+        thread[i].start()
+
+
+
